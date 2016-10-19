6d20dc2dff1c1831eaf5147e7a9644311b96173a
刘海铭 <hymanliu@raventech.com>
2016-10-19 11:22:50 +0800

    update log

--- src/main/java/com/haiyue/sbm/service/impl/DictServiceImpl.java
+++ src/main/java/com/haiyue/sbm/service/impl/DictServiceImpl.java
@@ -3,6 +3,8 @@ package com.haiyue.sbm.service.impl;
 
 import com.haiyue.sbm.mapper.DictMapper;
 import com.haiyue.sbm.service.DictService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,13 +21,19 @@ public class DictServiceImpl implements DictService {
     @Autowired
     private DictMapper dictMapper;
 
+
     @Override
     public List<String> getKeywordsByPinyin(String pinyin, String nature) throws Exception {
+        Logger log = LogManager.getLogger(DictServiceImpl.class.getName());
         Map<String, String> map = new HashMap<>();
         map.put("keyword", pinyin);
         map.put("nature", nature);
-        Long sum = dictMapper.getSum();
-        System.out.println(sum + "");
+        log.debug(map);
+        log.info(map);
+        log.warn(map);
+        log.error(map);
+//        Long sum = dictMapper.getSum();
+//        System.out.println(sum + "");
         return dictMapper.getLtsKeywordsByPinyin(map);
     }
 
